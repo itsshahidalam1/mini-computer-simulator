b845becadcbe3efb043e6cc65326c99293fb6f2f
shahid/windows <itsshahidalam@gmail.com>
2026-09-08 16:20:44 +0530

    odified the processor for lab 4

--- processor.c
+++ processor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define NP 4
 #include "processor.h"
 #include <string.h>
 #include <stdlib.h>
@@ -7,11 +8,11 @@ bool N = false;
 bool Z = false;
 bool C = false;
 bool V = false;
-int Register[256];
-int vectorRegister[32][8];
-int PC = 0, opcode, dest, src1, src2;
-extern unsigned char instruction_memory[256];
-extern unsigned char data_memory[4096];
+int Register[NP][256];
+int vectorRegister[NP][32][8];
+int PC[NP], opcode, dest, src1, src2;
+extern unsigned char instruction_memory[NP][256];
+extern unsigned char data_memory[NP][4096];
 
 int end_of_simulation = 0;
 void setFlags(int result, int op1, int op2, bool operation)
@@ -36,24 +37,31 @@ void setFlags(int result, int op1, int op2, bool operation)
 }
 void reset()
 {
-    for (int i = 0; i < 256; i++)
-        Register[i] = 0;
+    /*
+     * REGISTER IS 2D:
+     * Register[process][register]
+     *
+     * Reset all NP process register files.
+     */
+    for (int p = 0; p < NP; p++)
+        for (int i = 0; i < 256; i++)
+            Register[p][i] = 0;
 }
 
 void fetch()
 {
-    if (instruction_memory[PC] == 0)
+    if (instruction_memory[NP][PC] == 0)
     {
         printf("\n****************Program executed successfully***************\n");
         end_of_simulation = 1;
         return;
     }
-    opcode = instruction_memory[PC];
-    dest = instruction_memory[PC + 1];
-    src1 = instruction_memory[PC + 2];
-    src2 = instruction_memory[PC + 3];
+    opcode = instruction_memory[NP][PC];
+    dest = instruction_memory[NP][PC + 1];
+    src1 = instruction_memory[NP][PC + 2];
+    src2 = instruction_memory[NP][PC + 3];
 
-    PC = PC + 4;
+    PC[NP] = PC[NP] + 4;
 }
 
 void decode()
@@ -61,7 +69,7 @@ void decode()
     return;
 };
 
-void execute()
+void execute(int process)
 {
 
     if (end_of_simulation == 1)
@@ -73,55 +81,57 @@ void execute()
 
     case 0x01: // ADD
         printf("\n[EXECUTE] ADD    R%d = R%d + R%d\n", dest, src1, src2);
-        Register[dest] = Register[src1] + Register[src2];
-        setFlags(Register[dest], Register[src1], Register[src2], 0);
+        /* 2D register access: Register[process][register_number]. */
+        Register[process][dest] = Register[process][src1] + Register[process][src2];
+        setFlags(Register[process][dest], Register[process][src1], Register[process][src2], 0);
         break;
 
     case 0x02: // SUB
         printf("\n[EXECUTE] SUB    R%d = R%d - R%d\n", dest, src1, src2);
-        Register[dest] = Register[src1] - Register[src2];
-        setFlags(Register[dest], Register[src1], Register[src2], 1);
+        /* 2D register access: all operands belong to the same process. */
+        Register[process][dest] = Register[process][src1] - Register[process][src2];
+        setFlags(Register[process][dest], Register[process][src1], Register[process][src2], 1);
         printf("R1=%d R4=%d R15=%d Z=%d\n",
-       Register[src1], Register[src2],
-       Register[dest], Z);
+       Register[process][src1], Register[process][src2],
+       Register[process][dest], Z);
         break;
 
     case 0x03: // MUL
         printf("\n[EXECUTE] MUL    R%d = R%d * R%d\n", dest, src1, src2);
-        Register[dest] = Register[src1] * Register[src2];
+        Register[process][dest] = Register[process][src1] * Register[process][src2];
         break;
 
     case 0x04: // DIV
         printf("\n[EXECUTE] DIV    R%d = R%d / R%d\n", dest, src1, src2);
-        Register[dest] = Register[src1] / Register[src2];
+        Register[process][dest] = Register[process][src1] / Register[process][src2];
         break;
 
         /* ================= MEMORY ================= */
 
     case 0x05: // MEMORY READ
-        printf("\n[EXECUTE] LOAD   R%d = MEM[%d]\n", dest, Register[src2]);
+        printf("\n[EXECUTE] LOAD   R%d = MEM[%d]\n", dest, Register[process][src2]);
         {
-            int j = Register[src2];
+            int j = Register[process][src2];
             int value =
                 ((int)data_memory[j] << 24) | ((int)data_memory[j + 1] << 16) | ((int)data_memory[j + 2] << 8) | (int)data_memory[j + 3];
-            Register[dest] = value;
+            Register[process][dest] = value;
         }
         break;
 
     case 0x06: // MEMORY WRITE
         printf("\n[EXECUTE] STORE  MEM[%d] = R%d\n", dest, src2);
         {
-            int value = Register[src2];
-            data_memory[Register[dest]] = (unsigned char)(value >> 24) & 0xFF;
-            data_memory[Register[dest] + 1] = (unsigned char)(value >> 16) & 0xFF;
-            data_memory[Register[dest] + 2] = (unsigned char)(value >> 8) & 0xFF;
-            data_memory[Register[dest] + 3] = (unsigned char)value & 0xFF;
+            int value = Register[process][src2];
+            data_memory[Register[process][dest]] = (unsigned char)(value >> 24) & 0xFF;
+            data_memory[Register[process][dest] + 1] = (unsigned char)(value >> 16) & 0xFF;
+            data_memory[Register[process][dest] + 2] = (unsigned char)(value >> 8) & 0xFF;
+            data_memory[Register[process][dest] + 3] = (unsigned char)value & 0xFF;
         }
         break;
 
     case 0x07: // DATA MOVEMENT
         printf("\n[EXECUTE] MOV    R%d = R%d\n", dest, src2);
-        Register[dest] = Register[src2];
+        Register[process][dest] = Register[process][src2];
         break;
 
         /* ================= SCALAR CONSTANT ================= */
@@ -129,27 +139,27 @@ void execute()
     case 0x09: // ADD CONSTANT
         printf("\n[EXECUTE] ADDI   R%d = R%d + %d\n",
                dest, src1, src2);
-        Register[dest] = Register[src1] + src2;
-        setFlags(Register[dest], Register[src1], src2, 0);
+        Register[process][dest] = Register[process][src1] + src2;
+        setFlags(Register[process][dest], Register[process][src1], src2, 0);
         break;
 
     case 0x0A: // SUB CONSTANT
         printf("\n[EXECUTE] SUBI   R%d = R%d - %d\n",
                dest, src1, src2);
-        Register[dest] = Register[src1] - src2;
-        setFlags(Register[dest], Register[src1], src2, 1);
+        Register[process][dest] = Register[process][src1] - src2;
+        setFlags(Register[process][dest], Register[process][src1], src2, 1);
         break;
 
     case 0x0B: // MUL CONSTANT
         printf("\n[EXECUTE] MULI   R%d = R%d * %d\n",
                dest, src1, src2);
-        Register[dest] = Register[src1] * src2;
+        Register[process][dest] = Register[process][src1] * src2;
         break;
 
     case 0x0C: // DIV CONSTANT
         printf("\n[EXECUTE] DIVI   R%d = R%d / %d\n",
                dest, src1, src2);
-        Register[dest] = Register[src1] / src2;
+        Register[process][dest] = Register[process][src1] / src2;
         break;
 
     case 0x0D: // MEMORY READ CONSTANT ADDRESS
@@ -158,26 +168,26 @@ void execute()
             int j = src2;
             int value =
                 ((int)data_memory[j] << 24) | ((int)data_memory[j + 1] << 16) | ((int)data_memory[j + 2] << 8) | (int)data_memory[j + 3];
-            Register[dest] = value;
+            Register[process][dest] = value;
         }
 
         ;
         break;
 
     case 0x0E: // MEMORY WRITE CONSTANT
-        printf("\n[EXECUTE] STORE  MEM[%d] = %d\n", Register[dest], src2);
+        printf("\n[EXECUTE] STORE  MEM[%d] = %d\n", Register[process][dest], src2);
         {
             int value = src2;
-            data_memory[Register[dest]] = (unsigned char)(value >> 24) & 0xFF;
-            data_memory[Register[dest] + 1] = (unsigned char)(value >> 16) & 0xFF;
-            data_memory[Register[dest] + 2] = (unsigned char)(value >> 8) & 0xFF;
-            data_memory[Register[dest] + 3] = (unsigned char)value & 0xFF;
+            data_memory[Register[process][dest]] = (unsigned char)(value >> 24) & 0xFF;
+            data_memory[Register[process][dest] + 1] = (unsigned char)(value >> 16) & 0xFF;
+            data_memory[Register[process][dest] + 2] = (unsigned char)(value >> 8) & 0xFF;
+            data_memory[Register[process][dest] + 3] = (unsigned char)value & 0xFF;
         }
         break;
 
     case 0x0F: // DATA MOVEMENT CONSTANT
         printf("\n[EXECUTE] MOVI   R%d = %d\n", dest, src2);
-        Register[dest] = src2;
+        Register[process][dest] = src2;
         break;
 
         /* ================= BRANCH ================= */
@@ -187,7 +197,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (Z)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -196,7 +206,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!Z)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -206,7 +216,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (C)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -216,7 +226,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!C)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -226,7 +236,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (N)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -236,7 +246,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!N)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -246,7 +256,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (V)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -256,7 +266,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!V)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -266,7 +276,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (C && !Z)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -276,7 +286,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!C || Z)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -286,7 +296,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (N == V)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -296,7 +306,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (N != V)
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -306,7 +316,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (!Z && (N == V))
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -316,7 +326,7 @@ void execute()
         int offset = (int8_t)src2;
 
         if (Z || (N != V))
-            PC = PC + 4 * (offset - 1);
+            PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -325,7 +335,7 @@ void execute()
     {
         int offset = (int8_t)src2;
 
-        PC = PC + 4 * (offset - 1);
+        PC[process] = PC[process] + 4 * (offset - 1);
 
         break;
     }
@@ -337,7 +347,7 @@ void execute()
                dest, src1, src2);
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] + vectorRegister[src2][i];
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] + vectorRegister[process][src2][i];
         }
         break;
 
@@ -346,7 +356,7 @@ void execute()
                dest, src1, src2);
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] - vectorRegister[src2][i];
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] - vectorRegister[process][src2][i];
         }
         break;
 
@@ -355,7 +365,7 @@ void execute()
                dest, src1, src2);
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] * vectorRegister[src2][i];
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] * vectorRegister[process][src2][i];
         }
         break;
 
@@ -364,28 +374,28 @@ void execute()
     case 0x25: // VECTOR READ
         printf("\n[EXECUTE] VLOAD  V%d = VMEM[%d]\n",
                dest, src2);
-        int j = Register[src2];
+        int j = Register[process][src2];
         for (int i = 0; i < 8; i++)
         {
             int value =
                 ((int)data_memory[j] << 24) | ((int)data_memory[j + 1] << 16) | ((int)data_memory[j + 2] << 8) | (int)data_memory[j + 3];
-            vectorRegister[dest][i] = value;
+            vectorRegister[process][dest][i] = value;
             j += 4;
         }
 
         for (int i = 0; i < 8; i++)
-            printf("I : %d \n", vectorRegister[dest][i]);
+            printf("I : %d \n", vectorRegister[process][dest][i]);
         break;
 
     case 0x26: // VECTOR WRITE
         printf("\n[EXECUTE] VSTORE VMEM[%d] = V%d\n",
-               Register[dest], src2);
+               Register[process][dest], src2);
 
-        j = Register[dest];
+        j = Register[process][dest];
 
         for (int i = 0; i < 8; i++)
         {
-            int value = vectorRegister[src2][i];
+            int value = vectorRegister[process][src2][i];
             data_memory[j] = (unsigned char)(value >> 24) & 0xFF;
             data_memory[j + 1] = (unsigned char)(value >> 16) & 0xFF;
             data_memory[j + 2] = (unsigned char)(value >> 8) & 0xFF;
@@ -403,7 +413,7 @@ void execute()
                dest, src1, src2);
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] + src2;
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] + src2;
         }
         break;
 
@@ -412,7 +422,7 @@ void execute()
                dest, src1, src2);
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] - src2;
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] - src2;
         }
         break;
 
@@ -422,7 +432,7 @@ void execute()
 
         for (int i = 0; i < 8; i++)
         {
-            vectorRegister[dest][i] = vectorRegister[src1][i] * src2;
+            vectorRegister[process][dest][i] = vectorRegister[process][src1][i] * src2;
         }
 
         break;
@@ -436,19 +446,19 @@ void execute()
         {
             int value =
                 ((int)data_memory[j] << 24) | ((int)data_memory[j + 1] << 16) | ((int)data_memory[j + 2] << 8) | (int)data_memory[j + 3];
-            vectorRegister[dest][i] = value;
+            vectorRegister[process][dest][i] = value;
             j += 4;
         }
 
         for (int i = 0; i < 8; i++)
-            printf("I : %d \n", vectorRegister[dest][i]);
+            printf("I : %d \n", vectorRegister[process][dest][i]);
 
         break;
 
     case 0x2E: // VECTOR WRITE CONSTANT
         printf("\n[EXECUTE] VSTORE VMEM[%d] = %d\n",
-               Register[dest], src2);
-        j = Register[dest];
+               Register[process][dest], src2);
+        j = Register[process][dest];
 
         for (int i = 0; i < 8; i++)
         {
